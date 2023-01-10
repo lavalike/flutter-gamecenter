263aab4c2c93c7b44a3e88dd88b85da14d781ea2
zhen51.wang <zhen51.wang@tcl.com>
2023-01-10 14:44:11 +0800

    feat(flutter):打印FPS demo Closes ZHYYZCPT-143

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:collection';
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 import 'routes/app_bar_route.dart';
 import 'routes/reference_ui_route.dart';
@@ -18,6 +21,7 @@ void main() {
     debugDefaultTargetPlatformOverride = targetPlatform;
   }
   runApp(ReferenceUI());
+  SchedulerBinding.instance.addTimingsCallback(_onReportTimings);
 }
 
 class ReferenceUI extends StatelessWidget {
@@ -34,4 +38,48 @@ class ReferenceUI extends StatelessWidget {
       home: ReferenceUIRoute(child: AppBarRoute()),
     );
   }
+}
+
+const maxframes = 100; // 100 帧足够了，对于 60 fps 来说
+final lastFrames = ListQueue<FrameTiming>(maxframes);
+const REFRESH_RATE = 60;
+const frameInterval = const Duration(
+    microseconds: Duration.microsecondsPerSecond ~/ REFRESH_RATE);
+
+void _onReportTimings(List<FrameTiming> timings) {
+  // 把 Queue 当作堆栈用
+  for (FrameTiming timing in timings) {
+    lastFrames.addFirst(timing);
+  }
+
+  // 只保留 maxframes
+  while (lastFrames.length > maxframes) {
+    lastFrames.removeLast();
+  }
+
+  print("current fps: $fps");
+}
+
+double get fps {
+  var lastFramesSet = <FrameTiming>[];
+  for (FrameTiming timing in lastFrames) {
+    if (lastFramesSet.isEmpty) {
+      lastFramesSet.add(timing);
+    } else {
+      var lastStart =
+          lastFramesSet.last.timestampInMicroseconds(FramePhase.buildStart);
+      if (lastStart - timing.timestampInMicroseconds(FramePhase.rasterFinish) >
+          (frameInterval.inMicroseconds * 2)) {
+        // in different set
+        break;
+      }
+      lastFramesSet.add(timing);
+    }
+  }
+  var framesCount = lastFramesSet.length;
+  var costCount = lastFramesSet.map((t) {
+    // 耗时超过 frameInterval 会导致丢帧
+    return (t.totalSpan.inMicroseconds ~/ frameInterval.inMicroseconds) + 1;
+  }).fold(0, (a, b) => a + b);
+  return framesCount * REFRESH_RATE / costCount;
 }
